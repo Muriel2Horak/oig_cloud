a5347b0ccf194ffc3cd98bdab3245051ce085955
Martin Horak <martin.horak@o2.cz>
2026-02-14 22:17:07 +0100

    fix(flow): remove CSS width/height override on SVG connections layer

--- custom_components/oig_cloud/www_v2/src/ui/features/flow/canvas.ts
+++ custom_components/oig_cloud/www_v2/src/ui/features/flow/canvas.ts
@@ -69,8 +69,6 @@ export class OigFlowCanvas extends LitElement {
       position: absolute;
       top: 0;
       left: 0;
-      width: 100%;
-      height: 100%;
       pointer-events: none;
       z-index: 2;
     }
